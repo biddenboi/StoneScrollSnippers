71a1fc15db7b2220ba693eedee69acfde1b5b8f3
Yujin <yujinpetercho@gmail.com>
2024-01-12 20:53:50 -0600

    changed choice nums. to const values; getChosenImg
    changing numbers to const values for readability and getChosenImg used to retrieve
    correlating file link of numerical choice

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+const WIN = 1;
+const TIE = 0;
+const LOSE = -1;
+
+
 function playGame(playerChoice, botChoice) {
-    if (playerChoice == botChoice) return 0;
-    if (playerChoice < botChoice || playerChoice == SCISSORS && botChoice == ROCK) return -1;
-    return 1;
+    if (playerChoice == botChoice) return TIE;
+    if (playerChoice < botChoice || playerChoice == SCISSORS && botChoice == ROCK) return LOSE;
+    return WIN;
 }
 
+//if val is equal to 3, set as 2.
 function getRandomChoice() {
     let val;
-    return (val = Math.floor(Math.round() * 3) == 3) ? 2 : val;
+    return (val = Math.floor(Math.round() * 3) == 3) ? SCISSORS : val;
+}
+
+function getChosenImage(choice) {
+    if (choice == ROCK) return "./public/rock.png";
+    if (choice == SCISSORS) return "./public/scissors.png";
+    if (choice == PAPER) return "./public/paper.png";
 }
 
 const optionsButton = document.querySelector(".optionArea > button");
@@ -18,7 +30,17 @@ const displayBox = document.querySelector(".result");
 const playerInputBox = document.querySelector(".playerChoice");
 const computerInputBox = document.querySelector(".computerChoice");
 
+let currPlayerSelection = getRandomChoice();
+let currComputerSelection = getRandomChoice();
 
+/**
+ * player can change their selection anytime -> on eventlistener of playeth, then we compare and 
+ * execute
+ */
 
-const currPlayerSelection = 
-const currPlayerSelection = 
+playerInputBox.addEventListener("click", (event) => {
+    currPlayerSelection += 1;
+    currPlayerSelection %= 3;
+
+    
+})
